4d6df8e7ffbaf151de24e529c15d8c4c04c678a1
elifdev <elifdevpro@gmail.com>
2024-12-05 00:27:45 +0300

    update

--- CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
+++ CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
@@ -13,7 +13,7 @@ namespace CSharpEgitimKampi301.DataAccessLayer.Abstract
         void Insert(T entity);
 
         void Update(T entity);
-        void Delete(int id);
+        void Delete(T entity);
 
         List<T> GetAll();
 
